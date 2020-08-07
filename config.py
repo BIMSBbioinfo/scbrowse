7b3b224a7f13658ed78f3c628b50af23a273438d
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2020-08-07 17:08:38 +0200

    adapted for gunicorn config

---
+++ config.py
@@ -0,0 +1,7 @@
+
+bind = ':8000'
+workers = 4
+chdir = './src/scbrowse'
+accesslog = '/mnt/storage/wolfgang/wolfgang/src/scbrowse-logs/access_gunicorn.log'
+errorlog = '/mnt/storage/wolfgang/wolfgang/src/scbrowse-logs/error_gunicorn.log'
+#proc_name = 'scbrowse'
---
+++ startup.sh
@@ -0,0 +1,15 @@
+logdir=/mnt/storage/wolfgang/wolfgang/src/scbrowse-logs
+mkdir -p $logdir
+
+export SCBROWSE_EMBEDDING=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/cellannot.tsv
+export SCBROWSE_MATRIX=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/all.npz
+export SCBROWSE_REGIONS=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/tile.bed
+export SCBROWSE_GENES=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/danrer11_refgenes.bed
+export SCBROWSE_LOGS=$logdir/scbrowse.log
+
+# debugging with:
+# scbrowse
+
+# production with:
+gunicorn -c config.py cli:server
+
