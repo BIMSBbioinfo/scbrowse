7d016e946d920bbcb3bfe7d517817716c9ae3624
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2020-08-08 00:18:55 +0200

    removed comments

--- config.py
+++ config.py
@@ -3,7 +3,5 @@ import os
 bind = ':8000'
 workers = 4
 chdir = './src/scbrowse'
-#accesslog = '/mnt/storage/wolfgang/wolfgang/src/scbrowse-logs/access_gunicorn.log'
 accesslog = os.path.join(os.environ['SCBROWSE_LOGDIR'], 'access_gunicorn.log')
 errorlog = os.path.join(os.environ['SCBROWSE_LOGDIR'], 'error_gunicorn.log')
-#proc_name = 'singlecell'
