9c41729be1ddf2c5287c8c2c510643bdeea6c3c4
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2020-08-07 16:50:59 +0200

    added extra requirement gunicorn

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
+        'gunicorn': ['gunicorn']
         # eg:
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
