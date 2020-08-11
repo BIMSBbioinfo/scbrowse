fdc86547b64264d08d10f41da01d07d39b6b5c73
wkopp <wolfgang.kopp@mdc-berlin.de>
2020-08-11 20:29:46 +0200

    adapted to VM for scbrowse

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 rm -rf cache-directory
-export SCBROWSE_LOGDIR=/mnt/storage/wolfgang/wolfgang/src/scbrowse-logs
+export SCBROWSE_LOGDIR=/home/sa-l-scbrowse/logs
 mkdir -p $SCBROWSE_LOGDIR
 
-export SCBROWSE_EMBEDDING=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/cellannot.tsv
-export SCBROWSE_MATRIX=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/all.npz
-export SCBROWSE_REGIONS=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/tile.bed
-export SCBROWSE_GENES=/mnt/storage/wolfgang/wolfgang/src/python-scbrowse/data/danrer11_refgenes.bed
+export SCBROWSE_EMBEDDING=/home/sa-l-scbrowse/data/cellannot.tsv
+export SCBROWSE_MATRIX=/home/sa-l-scbrowse/data/all.npz
+export SCBROWSE_REGIONS=/home/sa-l-scbrowse/data/tile.bed
+export SCBROWSE_GENES=/home/sa-l-scbrowse/data/danrer11_refgenes.bed
 export SCBROWSE_LOGS=$SCBROWSE_LOGDIR/scbrowse.log
 
 # debugging with:
-# scbrowse
+#scbrowse
 
 # production with:
 gunicorn -c config.py cli:server
