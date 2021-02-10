a3aea103dec1a060ec429c2221f6a708bda80333
wkopp <wolfgang.kopp@mdc-berlin.de>
2021-02-10 23:47:33 +0100

    fixed data location

--- startup.bash
+++ startup.bash
@@ -10,7 +10,7 @@ rm -rf cache-directory
 export SCBROWSE_LOGDIR=/home/sa-l-scbrowse/logs
 mkdir -p $SCBROWSE_LOGDIR
 
-export SCBROWSE_MATRIX=data/scbrowse.h5ad
+export SCBROWSE_MATRIX=/home/sa-l-scbrowse/data/scbrowse.h5ad
 export SCBROWSE_GENES=/home/sa-l-scbrowse/data/danrer11_refgenes.bed
 export SCBROWSE_LOGS=$SCBROWSE_LOGDIR/scbrowse.log
 
